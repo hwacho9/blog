52772afd9766001fc51ed8fbd4fc958b5e630496
hwacho9 <csh77776@gmail.com>
2024-05-07 13:34:01 +0900

    feat:post filter

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,8 @@
+import FilterablePosts from "@/components/FilterablePosts";
 import { getAllPosts } from "@/service/posts";
 
 export default async function PostsPage() {
     const posts = await getAllPosts();
     const categories = [...new Set(posts.map((post) => post.category))];
-    return <p>포스트들!</p>;
+    return <FilterablePosts posts={posts} categories={categories} />;
 }
