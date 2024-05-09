7a09366de1c10561bae2d0e3d2d83c9f260aac82
hwacho9 <csh77776@gmail.com>
2024-05-09 12:57:43 +0900

    refactor:page section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" className={sans.className}>
-            <body className="flex flex-col w-full max-w-screen-3xl">
+            <body className="flex flex-col w-full max-w-screen-2xl mx-auto">
                 <Header />
                 <main className="grow">{children}</main>
                 <Footer />
