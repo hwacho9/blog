9df1b268499f44a3e2e329cab30c7a73f0cfe855
hwacho9 <csh77776@gmail.com>
2024-05-13 01:35:35 +0900

    feat:get next prev post

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -12,7 +12,10 @@ export type Post = {
 
 export type PostData = Post & {
     content: string;
+    next: Post | null;
+    prev: Post | null;
 };
+
 export async function getFeaturedPosts(): Promise<Post[]> {
     return getAllPosts() //
         .then((posts) => posts.filter((post) => post.featured));
@@ -38,12 +41,15 @@ export async function getPostData(fileName: string): Promise<PostData> {
         "posts",
         `${fileName}.md`
     );
-    const metadata = await getAllPosts() //
-        .then((posts) => posts.find((post) => post.path === fileName));
-    if (!metadata) {
+    const posts = await getAllPosts(); //
+    const post = posts.find((post) => post.path === fileName);
+
+    if (!post) {
         throw new Error(`Post not found: ${fileName}`);
     }
-
+    const index = posts.indexOf(post);
+    const next = index > 0 ? posts[index - 1] : null;
+    const prev = index < posts.length - 1 ? posts[index + 1] : null;
     const content = await readFile(filePath, "utf-8");
-    return { ...metadata, content };
+    return { ...post, content, next, prev };
 }
